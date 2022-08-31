2c25695e14a7255bf74644680159648bd7e013ed
Thiago Lira <thiago.slira@outlook.com>
2022-08-31 19:01:59 -0300

    Trabalhando no requisito 4, e apanhando

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// const colorPallete = document.getElementById('color-palette');
-// for (let index = 0; index < 4; index += 1) {
-//   colorPallete.createElement('div');
-// }
+// adicionando aleatoriedade nas cores da paleta
+const botaoAleatorio = document.getElementById('button-random-color');
+const paletaCores = document.getElementsByClassName('color');
+
+botaoAleatorio.addEventListener('click', function () {
+
+
+const caracteres = '0123456789ABCDEF';
+let hashtag = '#';
+for (let i = 0; i < 6; i += 1) {
+    hashtag += caracteres[Math.floor(Math.random() * caracteres.length)];
+    hashtag = colorRed;
+}
+}) console.log(generateColor());// fonte https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
