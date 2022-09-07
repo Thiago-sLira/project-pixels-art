09b4f6588c0681c899deda6604c62c132c4af706
Thiago Lira <thiago.slira@outlook.com>
2022-09-06 21:03:53 -0300

    Trabalhando nos requisitos 10 e 11

--- script.js
+++ script.js
@@ -48,9 +48,9 @@ function changeClass(event) {
   event.target.classList.add('selected');
 }
 
-
+function catColor() {
   for (let index = 0; index < paletaCores.length; index += 1) {
     paletaCores[index].addEventListener('click', changeClass);
   }
-
-
+}
+catColor();
